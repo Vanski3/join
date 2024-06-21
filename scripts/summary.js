a8856fb99dd0a6a8a19803aeead6feb1913d8bed
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-06-21 11:42:46 +0200

    add onclick="loadBoardContent()" to summary

--- scripts/summary.js
+++ scripts/summary.js
@@ -5,10 +5,10 @@ function renderSummary() {
    content.innerHTML = '';
    content.innerHTML += /*html*/ `
      <main class="summary-main">
-      <div class="headline-wrapper"><span class="headline">Good morning,</span><span id="username">Sofia Müller</span></div>
+      <div class="headline-wrapper"><span class="headline">Good morning,</span><span id="username"> Sofia Müller</span></div>
         <div class="summary-wrapper">
             <div class="row1">
-                <div class="date-urgent-task">
+                <div class="date-urgent-task" onclick="loadBoardContent()">
                     <div class="urgent-task">
                         <div class="img-amount-container">
                             <img src="./assets/img/summary/urgent-frame.svg" alt="urgent task" />
@@ -24,7 +24,7 @@ function renderSummary() {
                         </div>
                     </div>
                 </div>
-                <div class="task">
+                <div class="task" onclick="loadBoardContent()">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/Board.svg" alt="Task in Board" />
                         <span id="task-in-board">5</span>
@@ -33,28 +33,28 @@ function renderSummary() {
                 </div>
             </div>
             <div class="row2">
-                <div class="task task-todo">
+                <div class="task task-todo" onclick="loadBoardContent()">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/todo.svg" alt="Task To-do" />
                         <span id="todo">1</span>
                     </div>
                     <p>Task To-do</p>
                 </div>
-                <div class="task">
+                <div class="task" onclick="loadBoardContent()">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/In Progress.svg" alt="Task in Progress" />
                         <span id="in-progress">2</span>
                     </div>
                     <p>Task in Progress</p>
                 </div>
-                <div class="task">
+                <div class="task" onclick="loadBoardContent()">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/Awaiting feedback.svg" alt="Awaiting feedback" />
                         <span id="feedback">2</span>
                     </div>
                     <p>Awaiting Feedback</p>
                 </div>
-                <div class="task">
+                <div class="task" onclick="loadBoardContent()">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/Done.svg" alt="Task done">
                         <span id="done">1</span>
