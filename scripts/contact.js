ff613673105052b1a48b3560febde84d6fe1f67d
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-06-21 14:12:03 +0200

    add hover and selected state on conatct name

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,157 +1,148 @@
-let contactsData = [];
+let contactsData = []; 
 
 function loadContactsContent() {
-   fetch('/scripts/contact.json')
-      .then((response) => {
-         if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-         }
-         return response.json();
-      })
-      .then((data) => {
-         console.log('Contacts loaded:', data);
-         contactsData = data; // Store contacts data
-         renderContacts(contactsData);
-      })
-      .catch((error) => console.error('Error loading contacts:', error));
+  fetch('/scripts/contact.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Contacts loaded:', data);
+      contactsData = data; // Store contacts data
+      renderContacts(contactsData);
+    })
+    .catch(error => console.error('Error loading contacts:', error));
 }
 
 function renderContacts(contacts) {
-   const content = document.getElementById('mainContent');
-   content.innerHTML = '';
-   content.innerHTML += `
-       <div class="contacts-container">
-           <div class="contacts-list">
-               <button class="add-contact-btn">Add new contact</button>
-               <!-- Dynamisch geladene Kontakte -->
-           </div>
-           <div class="contact-detail">
-               <!-- Dynamisch geladene Kontaktdetails -->
-           </div>
-       </div>
-   `;
+  const content = document.getElementById('mainContent');
+  content.innerHTML = '';
+  content.innerHTML += `
+    <div class="contacts-container">
+      <div class="contacts-list">
+        <button class="add-contact-btn">Add new contact</button>
+      </div>
+      <div class="contact-detail">
+      </div>
+    </div>
+  `;
 
-   const contactsList = document.querySelector('.contacts-list');
+  const contactsList = document.querySelector('.contacts-list');
 
-   // Sortiere Kontakte nach Namen
+   // Sortiert Kontakte nach Namen
    contacts.sort((a, b) => a.name.localeCompare(b.name));
 
-   let currentLetter = '';
+  let currentLetter = '';
 
-   contacts.forEach((contact) => {
-       const firstLetter = contact.name[0].toUpperCase();
+  contacts.forEach(contact => {
+    const firstLetter = contact.name[0].toUpperCase();
 
-       if (firstLetter !== currentLetter) {
-           currentLetter = firstLetter;
-           const groupTitle = document.createElement('div');
-           groupTitle.classList.add('contact-group-title');
-           groupTitle.textContent = currentLetter;
-           contactsList.appendChild(groupTitle);
-       }
+    if (firstLetter !== currentLetter) {
+      currentLetter = firstLetter;
+      const groupTitle = document.createElement('div');
+      groupTitle.classList.add('contact-group-title');
+      groupTitle.textContent = currentLetter;
+      contactsList.appendChild(groupTitle);
+    }
 
-       const contactGroup = document.createElement('div');
-       contactGroup.classList.add('contact-group');
-
-       const contactElement = document.createElement('div');
-       contactElement.classList.add('contact');
-       contactElement.innerHTML = `
-           <div class="contact-initials" style="background-color: ${contact.color};">${contact.initials}</div>
-           <div class="contact-info">
-               <div class="contact-name">${contact.name}</div>
-               <div class="contact-email">${contact.email}</div>
-           </div>
-       `;
-
-       contactElement.addEventListener('click', () => {
-           renderContactDetail(contact);
-       });
-
-       contactGroup.appendChild(contactElement);
-       contactsList.appendChild(contactGroup);
-   });
-   removeButtonBackground();
-   changeContactsButtonBackground();
-
-   // Add the event listener for the add-contact-btn after rendering the content
-   let addContactButton = document.querySelector('.add-contact-btn');
-   if (addContactButton) {
-       addContactButton.addEventListener('click', openContactDialog);
-   }
-}
-
-function changeContactsButtonBackground(params) {
-   let contactButton = document.getElementById('contactButton');
-   contactButton.classList.add('menu-background');
-}
-
-function renderContactDetail(contact) {
-   const contactDetail = document.querySelector('.contact-detail');
-   contactDetail.innerHTML = /*html*/ `
-        <div class="contact-header">
-         <div class="name-initial-container">
-         <div class="contact-initials-large" style="background-color: ${contact.color};">${contact.initials}</div>
-         <div class="contact-name contact-name-detail">${contact.name}</div>
-         </div>
-            
-            <div class="contact-actions">
-                <button class="edit-btn">
-                    <img src="/assets/img/contact/edit.svg" alt="Edit" />
-                    Edit
-                </button>
-                <button class="delete-btn" onclick="deleteContact(${contact.id})">
-                    <img src="/assets/img/contact/delete.svg" alt="Delete" />
-                    Delete
-                </button>
-            </div>
-        </div>
-        <div class="contact-detail-info">
-            <h2>Contact Information</h2>
-            <p><strong>Email <br></strong> <a href="mailto:${contact.email}">${contact.email}</a></p><br>
-            <p><strong>Phone <br></strong> <a href="tel:${contact.phone}">${contact.phone}</a></p>
-        </div>
+    const contactElement = document.createElement('div');
+    contactElement.classList.add('contact');
+    contactElement.innerHTML = `
+      <div class="contact-initials" style="background-color: ${contact.color};">${contact.initials}</div>
+      <div class="contact-info">
+        <div class="contact-name">${contact.name}</div>
+        <div class="contact-email">${contact.email}</div>
+      </div>
     `;
+
+    contactElement.addEventListener('click', () => {
+      renderContactDetail(contactElement, contact);
+    });
+
+    contactsList.appendChild(contactElement);
+  });
+
+  const addContactButton = document.querySelector('.add-contact-btn');
+  if (addContactButton) {
+    addContactButton.addEventListener('click', openContactDialog);
+  }
+}
+
+function renderContactDetail(contactElement, contact) {
+  document.querySelectorAll('.contact').forEach(element => {
+    element.classList.remove('selected');
+  });
+
+  contactElement.classList.add('selected');
+
+  const contactDetail = document.querySelector('.contact-detail');
+  contactDetail.innerHTML = `
+    <div class="contact-header">
+      <div class="name-initial-container">
+        <div class="contact-initials-large" style="background-color: ${contact.color};">${contact.initials}</div>
+        <div class="contact-name contact-name-detail">${contact.name}</div>
+      </div>
+      <div class="contact-actions">
+        <button class="edit-btn">
+          <img src="/assets/img/contact/edit.svg" alt="Edit" />
+          Edit
+        </button>
+        <button class="delete-btn" onclick="deleteContact(${contact.id})">
+          <img src="/assets/img/contact/delete.svg" alt="Delete" />
+          Delete
+        </button>
+      </div>
+    </div>
+    <div class="contact-detail-info">
+      <h2>Contact Information</h2>
+      <p><strong>Email <br></strong> <a href="mailto:${contact.email}">${contact.email}</a></p><br>
+      <p><strong>Phone <br></strong> <a href="tel:${contact.phone}">${contact.phone}</a></p>
+    </div>
+  `;
 }
 
 function deleteContact(id) {
-   const index = contactsData.findIndex(contact => contact.id === id);
-   if (index !== -1) {
-      contactsData.splice(index, 1);
-      renderContacts(contactsData);
-      document.querySelector('.contact-detail').innerHTML = '';
-   }
+  const index = contactsData.findIndex(contact => contact.id === id);
+  if (index !== -1) {
+    contactsData.splice(index, 1);
+    renderContacts(contactsData);
+    document.querySelector('.contact-detail').innerHTML = '';
+  }
 }
 
 function openContactDialog() {
-    let contactPopup = document.getElementById('contact-dialog-container');
-    let contactDialog = document.querySelector('#contact-dialog-container .task-dialog');
+  const contactPopup = document.getElementById('contact-dialog-container');
+  const contactDialog = document.querySelector('#contact-dialog-container .task-dialog');
 
-    contactPopup.style.display = 'unset';
-    setTimeout(function () {
-        contactDialog.style.right = '0';
-    }, 50);
+  contactPopup.style.display = 'unset';
+  setTimeout(() => {
+    contactDialog.style.right = '0';
+  }, 50);
 }
 
 function closeContactDialog() {
-    let contactDialog = document.querySelector('#contact-dialog-container .task-dialog');
+  const contactDialog = document.querySelector('#contact-dialog-container .task-dialog');
 
-    contactDialog.style.right = '-600px';
-    setTimeout(function () {
-        document.getElementById('contact-dialog-container').style.display = 'none';
-    }, 300);
+  contactDialog.style.right = '-600px';
+  setTimeout(() => {
+    document.getElementById('contact-dialog-container').style.display = 'none';
+  }, 300);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    let addContactButton = document.querySelector('.add-contact-btn');
+document.addEventListener('DOMContentLoaded', () => {
+  const addContactButton = document.querySelector('.add-contact-btn');
 
-    if (addContactButton) {
-        addContactButton.addEventListener('click', openContactDialog);
+  if (addContactButton) {
+    addContactButton.addEventListener('click', openContactDialog);
+  }
+
+  document.getElementById('contact-dialog-container').addEventListener('click', event => {
+    const contactDialog = document.querySelector('#contact-dialog-container .task-dialog');
+
+    if (!contactDialog.contains(event.target)) {
+      closeContactDialog();
     }
-
-    document.getElementById('contact-dialog-container').addEventListener('click', function (event) {
-        let contactDialog = document.querySelector('#contact-dialog-container .task-dialog');
-
-        if (!contactDialog.contains(event.target)) {
-            closeContactDialog();
-        }
-    });
+  });
 });
