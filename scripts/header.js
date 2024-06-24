d06173ea2ea381484c896c84c1263f9b376eb91e
Pascal Nehlsen <pascaldev92@gmail.com>
2024-06-24 12:53:18 +0200

    set sessionStorage to header

--- scripts/header.js
+++ scripts/header.js
@@ -1,21 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-   let userLogoHeader = document.getElementById('UserLogoHeader');
-   let userHeaderPopup = document.getElementById('UserHeaderPopup');
+  let userLogoHeader = document.getElementById('logoInitials');
+  let userHeaderPopup = document.getElementById('userHeaderPopup');
 
-   userLogoHeader.addEventListener('click', function (event) {
-      event.stopPropagation(); // Prevent the click from bubbling up to the document
-      if (userHeaderPopup.style.display === 'none' || userHeaderPopup.style.display === '') {
-         userHeaderPopup.style.display = 'flex';
-      } else {
-         userHeaderPopup.style.display = 'none';
-      }
-   });
-
-   document.addEventListener('click', function () {
+  userLogoHeader.addEventListener('click', function (event) {
+    event.stopPropagation(); // Prevent the click from bubbling up to the document
+    if (userHeaderPopup.style.display === 'none' || userHeaderPopup.style.display === '') {
+      userHeaderPopup.style.display = 'flex';
+    } else {
       userHeaderPopup.style.display = 'none';
-   });
+    }
+  });
 
-   userHeaderPopup.addEventListener('click', function (event) {
-      event.stopPropagation(); // Prevent the click from bubbling up to the document
-   });
+  document.addEventListener('click', function () {
+    userHeaderPopup.style.display = 'none';
+  });
+
+  userHeaderPopup.addEventListener('click', function (event) {
+    event.stopPropagation(); // Prevent the click from bubbling up to the document
+  });
 });
+
+function logOut() {
+  sessionStorage.clear();
+  window.location.href = 'index.html';
+}
+
+sessionStorageToHeader();
+
+function sessionStorageToHeader() {
+  let logo = document.getElementById('logoInitials');
+  let initials = sessionStorage.getItem('initials');
+  logo.innerHTML += `${initials}`;
+}
