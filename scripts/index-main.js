99c43ee012452a01170ad49de49d3f6b5d7e9737
Pascal Nehlsen <pascaldev92@gmail.com>
2024-06-24 15:39:43 +0200

    added search function to popup

--- scripts/index-main.js
+++ scripts/index-main.js
@@ -1,21 +1,21 @@
 function removeButtonBackground(params) {
-   let summaryButton = document.getElementById('summaryButton');
-   summaryButton.classList.remove('menu-background');
-   let addTaskButton = document.getElementById('addTaskButton');
-   addTaskButton.classList.remove('menu-background');
-   let boardButton = document.getElementById('boardButton');
-   boardButton.classList.remove('menu-background');
-   let contactButton = document.getElementById('contactButton');
-   contactButton.classList.remove('menu-background');
+  let summaryButton = document.getElementById('summaryButton');
+  summaryButton.classList.remove('menu-background');
+  let addTaskButton = document.getElementById('addTaskButton');
+  addTaskButton.classList.remove('menu-background');
+  let boardButton = document.getElementById('boardButton');
+  boardButton.classList.remove('menu-background');
+  let contactButton = document.getElementById('contactButton');
+  contactButton.classList.remove('menu-background');
 }
 
 function removeColorSideBar(params) {
-   let sidebarImgSummary = document.getElementById('sidebarImgSummary');
-   sidebarImgSummary.classList.remove('color-img-sidebar');
-   let sidebarImgAddTask = document.getElementById('sidebarImgAddTask');
-   sidebarImgAddTask.classList.remove('color-img-sidebar');
-   let sidebarImgBoard = document.getElementById('sidebarImgBoard');
-   sidebarImgBoard.classList.remove('color-img-sidebar');
-   let sidebarImgContact = document.getElementById('sidebarImgContact');
-   sidebarImgContact.classList.remove('color-img-sidebar');
+  let sidebarImgSummary = document.getElementById('sidebarImgSummary');
+  sidebarImgSummary.classList.remove('color-img-sidebar');
+  let sidebarImgAddTask = document.getElementById('sidebarImgAddTask');
+  sidebarImgAddTask.classList.remove('color-img-sidebar');
+  let sidebarImgBoard = document.getElementById('sidebarImgBoard');
+  sidebarImgBoard.classList.remove('color-img-sidebar');
+  let sidebarImgContact = document.getElementById('sidebarImgContact');
+  sidebarImgContact.classList.remove('color-img-sidebar');
 }
