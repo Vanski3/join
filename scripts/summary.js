b2da1ff1db9c6949b17babd5b95948fdb9439ba6
Pascal Nehlsen <pascaldev92@gmail.com>
2024-06-25 22:09:14 +0200

    finished summary render

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,11 +1,12 @@
 // renderSummary()
+
 function renderSummary() {
-   const name = sessionStorage.getItem('name') ?? 'Guest';
-   const content = document.querySelector('.main-content');
-   content.innerHTML = '';
-   content.innerHTML += /*html*/ `
+  const name = sessionStorage.getItem('name') ?? 'Guest';
+  const content = document.querySelector('.main-content');
+  content.innerHTML = '';
+  content.innerHTML += /*html*/ `
      <main class="summary-main">
-      <div class="headline-wrapper"><span class="headline">Good morning,</span><span id="username"> ${name}</span></div>
+      <div class="headline-wrapper"><span class="headline" id="daytime">Good morning,</span><span id="username"> ${name}</span></div>
         <div class="summary-wrapper">
             <div class="row1">
                 <div class="date-urgent-task" onclick="loadBoardContent()">
@@ -24,7 +25,7 @@ function renderSummary() {
                         </div>
                     </div>
                 </div>
-                <div class="task" onclick="loadBoardContent()">
+                <div class="task" onclick="loadBoardContent('mainContent')">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/Board.svg" alt="Task in Board" />
                         <span id="task-in-board">5</span>
@@ -33,28 +34,28 @@ function renderSummary() {
                 </div>
             </div>
             <div class="row2">
-                <div class="task task-todo" onclick="loadBoardContent()">
+                <div class="task task-todo" onclick="loadBoardContent('board-todo')">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/todo.svg" alt="Task To-do" />
                         <span id="todo">1</span>
                     </div>
                     <p>Task To-do</p>
                 </div>
-                <div class="task" onclick="loadBoardContent()">
+                <div class="task" onclick="loadBoardContent('board-progress')">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/In Progress.svg" alt="Task in Progress" />
                         <span id="in-progress">2</span>
                     </div>
                     <p>Task in Progress</p>
                 </div>
-                <div class="task" onclick="loadBoardContent()">
+                <div class="task" onclick="loadBoardContent('board-feedback')">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/Awaiting feedback.svg" alt="Awaiting feedback" />
                         <span id="feedback">2</span>
                     </div>
                     <p>Awaiting Feedback</p>
                 </div>
-                <div class="task" onclick="loadBoardContent()">
+                <div class="task" onclick="loadBoardContent('board-done')">
                     <div class="img-amount-container">
                         <img src="./assets/img/summary/Done.svg" alt="Task done">
                         <span id="done">1</span>
@@ -64,19 +65,105 @@ function renderSummary() {
             </div>
         </div>
     </main>`;
-   removeBackgroundLowerSidebar();
-   removeButtonBackground();
-   changeSummaryButtonBackground();
-   removeColorSideBar();
-   changeColorSideSummary();
+  removeBackgroundLowerSidebar();
+  removeButtonBackground();
+  changeSummaryButtonBackground();
+  removeColorSideBar();
+  changeColorSideSummary();
+  renderTaskAmount();
+}
+
+function renderTaskAmount() {
+  let taskNumber = document.getElementById('task-in-board');
+
+  let taskAmount = tasks.taskStatus.length;
+  taskNumber.innerHTML = `${taskAmount}`;
+  renderTaskToDoAmount();
+  renderTaskinProgressAmount();
+  renderTaskAwaitingAmount();
+  renderTaskDoneAmount();
+  renderDueDate();
+  renderUrgendTaskAmount();
+  renderGetGreeting();
+}
+
+function renderGetGreeting() {
+  let daytime = document.getElementById('daytime');
+  let now = new Date();
+  let hour = now.getHours();
+
+  if (hour >= 5 && hour < 12) {
+    daytime.innerHTML = 'Good Morning,';
+  } else if (hour >= 12 && hour < 17) {
+    daytime.innerHTML = 'Good Afternoon,';
+  } else if (hour >= 17 && hour < 21) {
+    daytime.innerHTML = 'Good Evening,';
+  } else {
+    daytime.innerHTML = 'Good Evening,';
+  }
+}
+
+function renderUrgendTaskAmount() {
+  let taskNumber = document.getElementById('urgent');
+  let CountThrees = tasks.priority.filter((task) => task === 'urgent').length;
+
+  taskNumber.innerHTML = `${CountThrees}`;
+}
+
+function renderDueDate() {
+  let dateContainer = document.getElementById('date');
+  let earliestDate = tasks.dueDate.reduce((earliest, current) => {
+    return new Date(current) < new Date(earliest) ? current : earliest;
+  });
+  let date = new Date(earliestDate);
+  let options = { year: 'numeric', month: 'long', day: 'numeric' };
+  let formattedDate = date.toLocaleDateString('en-US', options);
+
+  dateContainer.innerHTML = `${formattedDate}`;
+}
+
+function renderTaskDoneAmount() {
+  let taskNumber = document.getElementById('done');
+  let CountThrees = tasks.taskStatus.filter((task) => task === '3').length;
+
+  taskNumber.innerHTML = `${CountThrees}`;
+}
+
+function renderTaskAwaitingAmount() {
+  let taskNumber = document.getElementById('feedback');
+  let countTwos = tasks.taskStatus.filter((task) => task === '2').length;
+
+  taskNumber.innerHTML = `${countTwos}`;
+}
+
+function renderTaskinProgressAmount() {
+  let taskNumber = document.getElementById('todo');
+  let countZeroes = tasks.taskStatus.filter((task) => task === '0').length;
+
+  taskNumber.innerHTML = `${countZeroes}`;
+}
+
+function renderTaskToDoAmount() {
+  let taskNumber = document.getElementById('in-progress');
+  let countOnes = tasks.taskStatus.filter((task) => task === '1').length;
+
+  taskNumber.innerHTML = `${countOnes}`;
 }
 
 function changeSummaryButtonBackground(params) {
-   let summaryButton = document.getElementById('summaryButton');
-   summaryButton.classList.add('menu-background');
+  let summaryButton = document.getElementById('summaryButton');
+  summaryButton.classList.add('menu-background');
 }
 
 function changeColorSideSummary(params) {
-   document.getElementById('sidebarImgSummary').classList.add('color-img-sidebar');
-   document.getElementById('fontSummarySidebar').classList.add('menu-row-font');
+  document.getElementById('sidebarImgSummary').classList.add('color-img-sidebar');
+  document.getElementById('fontSummarySidebar').classList.add('menu-row-font');
+}
+
+function scrollToSection(sectionId) {
+  let targetElement = document.getElementById(sectionId);
+
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
